7f60b781559168a11f17fea1be297035c85c4e9f
blackeagle94 <blackeagle4894@gmail.com>
2022-02-17 19:08:13 +0100

    implemented amazon scrapper

--- app.js
+++ app.js
@@ -1,48 +1,55 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-const cors = require('cors')
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+const cors = require("cors");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-const besiktasPlayersRouter = require('./routes/besiktasPlayers.routes')
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+const besiktasPlayersRouter = require("./routes/besiktasPlayers.routes");
 
 var app = express();
 
 //db connection
-require('./config/db.config')();
+require("./config/db.config")();
+
+// amazon web scrapper
+const { search } = require("./amazon-scarpper/amazon");
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors())
+app.use(express.static(path.join(__dirname, "public")));
+app.use(cors());
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/api', besiktasPlayersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/api", besiktasPlayersRouter);
+app.get("/amazon", async (req, res) => {
+  const { title } = req.query;
+  await search(title, res);
+});
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
